0a8e566d1acbc843c282c8da3984d15402d737a9
parva3105 <parvashah310501@gmail.com>
2021-10-06 02:45:23 +0530

    Completed scroll styling

--- js/script.js
+++ js/script.js
@@ -131,5 +131,28 @@ function scrollUp(){
     }
     console.log("Scroll up being called and working");
 }
-
 window.addEventListener('scroll',scrollUp)
+
+//======================= Scroll Section Active Highlight ============================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY < sectionTop + sectionHeight ) {
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId  + ']').classList.add('active-link')
+        }else{
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId  + ']').classList.remove('active-link')
+        }
+    })
+
+    console.log("Section highlight working!");
+}
+window.addEventListener('scroll',scrollActive)
+
+
